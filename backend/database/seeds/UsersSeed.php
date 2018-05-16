f3a9d6eaff653a506de7669c434afa8265be2bfd
Ian Beemsterboer <s1104168@student.hsleiden.nl>
2018-05-16 12:31:38 +0200

    Migration for user added. Sample users in db loaded

---
+++ backend/database/seeds/UsersSeed.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($x = 0; $x <= 100; $x++) {
+            
+        
+        DB::table('user')->insert([
+            'user_username' => str_random(10),
+            'user_email' => str_random(10).'@gmail.com',
+            'user_password' => str_random(255),
+            'user_first_name' => str_random(10),
+            'user_last_name' => str_random(14),
+            'user_description' => str_random(40),
+            'user_salt' => str_random(126),
+            'user_force_pw_change' => 0,
+            'user_force_name_change' => 0,
+            'user_guest' => 0 
+        ]);
+    }
+    }
+}
