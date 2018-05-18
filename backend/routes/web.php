e45de49ddb53fdc4687e261224d7eee5286ccdff
Joppe <s1103172@student.hsleiden.com>
2018-05-18 12:12:32 +0200

    added basic database communication

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -25,14 +25,16 @@ $router->post('/joke', function (Request $request) use ($router) {
     $joke = "Which pet makes the most noise? A trumpet.";
 
 
-//    $name = $request->input('name');
-//    $date = $request->input('date');
-//
-//    echo $name;
-//    echo $date;
+    $results = \DB::select("SELECT * FROM tournament");
 
     $input = $request->all();
     echo implode(" ", $input);
 
+    var_dump($results);
+
     return $joke;
+});
+
+$router->post('/tournament', function () use ($router) {
+    return $router->app->version();
 });
