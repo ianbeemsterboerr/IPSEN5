fc927a67295166b0fb704009536f3dbcb9883db5
Joppe <s1103172@student.hsleiden.com>
2018-05-30 13:00:31 +0200

    input validation fully functional

--- frontend/src/app/tournament/tournament-new/tournament-new.component.ts
+++ frontend/src/app/tournament/tournament-new/tournament-new.component.ts
@@ -5,8 +5,9 @@ import {Params, Router} from '@angular/router';
 import {ApiService} from '../../shared/api.service';
 import {GameService} from '../../games/game.service';
 import {Tournament} from '../../shared/model/tournament';
-import {FormControl, FormGroup, FormsModule, Validators} from '@angular/forms';
+import {AbstractControl, FormControl, FormGroup, FormsModule, ValidatorFn, Validators} from '@angular/forms';
 import {ReactiveFormsModule} from '@angular/forms';
+import {ValidateFn} from 'codelyzer/walkerFactory/walkerFn';
 
 @Component({
   selector: 'app-tournament-new',
@@ -18,49 +19,42 @@ export class TournamentNewComponent implements OnInit {
   constructor(private api: ApiService, private gameService: GameService, private router: Router) { }
 
   submitDisabled = true;
-  newTournament = new Tournament(0, 0, "", "", "",
-    "", "", null, null, null, null,
+  newTournament = new Tournament(0, 0, '', 'Single elimination',
+    'Invite only', '', '', null, null, null, null,
     null, null);
   tournamentForm: FormGroup;
   ngOnInit() {
     this.tournamentForm = new FormGroup({
       'name': new FormControl(this.newTournament.name, [
-        Validators.required,
-        Validators.minLength(8)
+        Validators.required
       ]),
       'date': new FormControl(this.newTournament.tournament_Start, [
-        Validators.required
+        Validators.required,
+        this.tournamentStartValidator()
       ]),
+      'tournament_typename': new FormControl(this.newTournament.tournament_typename, []),
       'teamsize': new FormControl(this.newTournament.max_team_size, [
-        Validators.required
+        Validators.required,
+        this.teamsizeValidator(1)
       ]),
+      'signup_typename': new FormControl(this.newTournament.signup_typename, []),
       'signupStart': new FormControl(this.newTournament.signup_start, [
-        Validators.required
+        Validators.required,
+        this.signupStartValidator()
       ]),
       'signupEnd': new FormControl(this.newTournament.signup_end, [
-        Validators.required
-      ])
-    });
-  }
-  updateButton() {
-    let valid = true;
-    const values = document.getElementById('form');
-    for (let i = 0; i < values.children.length - 1; i++) {
-      if (values[i].value.length === 0) {
-        valid = false;
-      }
-    }
+        Validators.required,
+        this.signupEndValidator()
+      ]),
+      'description': new FormControl(this.newTournament.description, [])
 
-    this.submitDisabled = !valid;
+    });
   }
   get name() { return this.tournamentForm.get('name'); }
   get date() { return this.tournamentForm.get('date'); }
   get teamsize() { return this.tournamentForm.get('teamsize'); }
   get signupStart() { return this.tournamentForm.get('signupStart'); }
   get signupEnd() { return this.tournamentForm.get('signupEnd'); }
-
-
-
   submitForm() {
     const values = document.getElementById('form');
     console.log('Submitting tournament: ' + values[0].value);
@@ -87,4 +81,84 @@ export class TournamentNewComponent implements OnInit {
 
   }
 
+  teamsizeValidator(allowedSize: number): ValidatorFn {
+    return (control: AbstractControl): {[key: string]: any} => {
+      const invalid = control.value < allowedSize && control.value != null;
+      console.log(control.value);
+      console.log(invalid);
+      console.log('Name: ' + this.newTournament.name);
+      return invalid ? {'teamsizeInvalid': {value: control.value}} : null;
+    };
+  }
+
+  tournamentStartValidator(): ValidatorFn {
+    return (control: AbstractControl): {[key: string]: any} => {
+      const empty = control.value == null;
+      const today = new Date();
+      const input = new Date(control.value + 'T23:59:59');
+      const signupStart = new Date(this.newTournament.signup_start + 'T00:00:00');
+      const signupEnd = new Date(this.newTournament.signup_end + 'T23:59:59');
+
+      const invalidPast = input < today && !empty;
+      const invalidSignupStart = input < signupStart && !empty;
+      const invalidSignupEnd = input < signupEnd && !empty;
+      if (invalidPast) {
+        return {'futureDateInvalid': {value: control.value}};
+      } else if (invalidSignupStart) {
+        return {'signupStartInvalid': {value: control.value}};
+      } else if (invalidSignupEnd) {
+        return {'signupEndInvalid': {value: control.value}};
+      } else {
+        return null;
+      }
+    };
+  }
+
+  signupStartValidator(): ValidatorFn {
+    return (control: AbstractControl): {[key: string]: any} => {
+      const empty = control.value == null;
+      const today = new Date();
+      const input = new Date(control.value + 'T23:59:59');
+      const signupEnd = new Date(this.newTournament.signup_end + 'T23:59:59');
+      const tournamentStart = new Date(this.newTournament.tournament_Start + 'T23:59:59')
+
+      const invalidPast = input < today && !empty;
+      const invalidTournament = input > tournamentStart && !empty;
+      const invalidSignupEnd = input > signupEnd && !empty;
+      if (invalidPast) {
+        return {'futureDateInvalid': {value: control.value}};
+      } else if (invalidTournament) {
+        return {'tournamentInvalid': {value: control.value}};
+      } else if (invalidSignupEnd) {
+        return {'signupEndInvalid': {value: control.value}};
+      } else {
+        return null;
+      }
+    };
+  }
+
+  signupEndValidator(): ValidatorFn {
+    return (control: AbstractControl): {[key: string]: any} => {
+      const empty = control.value == null;
+      const today = new Date();
+      const input = new Date(control.value + 'T23:59:59');
+      const signupStart = new Date(this.newTournament.signup_start + 'T00:00:00');
+      const tournamentStart = new Date(this.newTournament.tournament_Start + 'T23:59:59')
+
+      const invalidPast = input < today && !empty;
+      const invalidSignupStart = input < signupStart && !empty;
+      const invalidTournament = input > tournamentStart && !empty;
+      if (invalidPast) {
+        return {'futureDateInvalid': {value: control.value}};
+      } else if (invalidSignupStart) {
+        return {'signupStartInvalid': {value: control.value}};
+      } else if (invalidTournament) {
+        return {'tournamentInvalid': {value: control.value}};
+      } else {
+        return null;
+      }
+    };
+  }
 }
+
+
