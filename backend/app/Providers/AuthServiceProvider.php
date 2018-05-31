d838a714433f5816b4ec7f991c03c1d20dd68df7
Ian Beemsterboer <s1104168@student.hsleiden.nl>
2018-05-31 09:01:07 +0200

    merge nu wel

--- backend/app/Providers/AuthServiceProvider.php
+++ backend/app/Providers/AuthServiceProvider.php
@@ -31,18 +31,11 @@ class AuthServiceProvider extends ServiceProvider
         // should return either a User instance or null. You're free to obtain
         // the User instance via an API token or any other method necessary.
 
-<<<<<<< HEAD
         $this->app['auth']->viaRequest('api', function ($request) {
             if ($request->header('Authorization')) {
                 $user;
                 $token = str_replace('Bearer ','', $request->header('Authorization'));
                 $jwt = JWT::decode($token, "JWT", array('HS256'));
-=======
-        $this->app['auth']->viaRequest('api', function (Request $request) {
-            if ($request->header('bearer')) {
-                $user = null;
-                $jwt = JWT::decode($request->header('bearer'), "JWT", array('HS256'));
->>>>>>> 53be126fbb0011fe2bef635fe2cfd39562f31525
                 if ($jwt->isadmin)
                 {
                     $user =  User::whereUserUsername($jwt->user_username)->first();
