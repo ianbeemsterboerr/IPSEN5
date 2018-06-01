709301b1af5facd6f9426c1defe6b0232567c9ec
Joppe <s1103172@student.hsleiden.com>
2018-06-01 15:02:38 +0200

    added prep for additional members in tournament status

--- frontend/src/app/shared/model/team.ts
+++ frontend/src/app/shared/model/team.ts
@@ -5,6 +5,7 @@ export class Team {
   constructor (
     public players: Player[],
     public name: string,
-    public team_leader?: User
+    public team_leader?: User,
+    public team_members?: User[]
   ) {}
 }
