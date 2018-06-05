a1d14f125cfa358cc48579a0bfd59f0016931a5c
Joppe <s1103172@student.hsleiden.com>
2018-06-05 11:49:39 +0200

    added dynamic tournamentoptions/organizerusername based on current user == organizer

--- frontend/src/app/tournament/tournament/tournament.component.ts
+++ frontend/src/app/tournament/tournament/tournament.component.ts
@@ -14,6 +14,7 @@ import {Team} from '../../shared/model/team';
 })
 export class TournamentComponent implements OnInit {
     public tournament: Tournament;
+    public isOrganizer: boolean;
 
     public today: Date = new Date();
     public start: Date;
@@ -30,7 +31,11 @@ export class TournamentComponent implements OnInit {
 
 
             this.tournamentService.getTournament(id).subscribe(
-                tournament => {this.tournament = tournament; this.start = new Date(this.tournament.signup_end)},
+                tournament => {
+                  this.tournament = tournament;
+                  this.start = new Date(this.tournament.signup_end);
+                  this.isOrganizer = localStorage.getItem('activeUserId') === this.tournament.organizer_user_id.toString();
+                },
                 error => {/*todo: resolve error case*/},
                 () => {}
             );
